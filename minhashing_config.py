670db7b069cdb5620ccf5f85a272fe75e7d2c5d4
Pravar Mahajan <mahajan.89@osu.edu>
2016-12-16 19:10:39 -0500

    adding final report

--- minhashing_config.py
+++
@@ -1,3 +0,0 @@
-shingles_path = 'data/shingles'
-hashes_filename = 'hashes.dat'
-jaccard_similarity_file = 'jacc.dat'
--- preprocessing_config.py
+++
@@ -1,2 +0,0 @@
-input_data_dir = "data/input" #This is where all the input sgm files are stored
-output_data_dir = "data/output" #Extracted features will be stored here
